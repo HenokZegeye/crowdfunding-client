5a425e858bf36f179f38d1524451cec816d7ffe8
HenokZeg <henokzegeye1990@gmail.com>
2020-11-11 16:43:25 +0300

    campaign card generated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CampaignCardComponent } from './components/campaign-card/campaign-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CampaignCardComponent],
   imports: [
     CommonModule
   ]
