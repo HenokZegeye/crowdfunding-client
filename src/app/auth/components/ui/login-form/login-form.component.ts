499a6845c14dee13fd47122164cc30814048e3d0
HenokZeg <henokzegeye1990@gmail.com>
2020-12-12 21:35:53 +0300

    Login components added

--- src/app/auth/components/ui/login-form/login-form.component.ts
+++ src/app/auth/components/ui/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GenericValidator } from 'src/app/shared/validators/generic.validator';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+  @Output() formSubmit = new EventEmitter();
+  @Input() loading: boolean;
+  hide = true;
 
-  constructor() { }
+  form: FormGroup;
+  displayMessage: { [key: string]: string } = {};
+  private genericValidator: GenericValidator;
+  private readonly validationMessages: { [key: string]: { [key: string]: string } };
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.validationMessages = {
+      email: {
+        required: 'Email is required.',
+        email: 'Invalid email format.'
+      },
+      password: {
+        required: 'Password is required.',
+        minlength: 'Password should be more than 6 characters.'
+      }
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
 
   ngOnInit(): void {
+    this.form.valueChanges.subscribe(
+      () => this.displayMessage = this.genericValidator.processMessages(this.form)
+    );
+  }
+
+  blur(): void {
+    this.displayMessage = this.genericValidator.processMessages(this.form);
+  }
+
+  onLogin() {
+    this.formSubmit.emit(this.form.value);
   }
 
 }
