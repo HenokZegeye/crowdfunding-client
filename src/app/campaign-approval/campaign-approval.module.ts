e6f151c0aadde3fc1e44bf1b63d02af72af59150
HenokZeg <henokzegeye1990@gmail.com>
2020-12-12 23:13:57 +0300

    Campaign approval module generated

---
+++ src/app/campaign-approval/campaign-approval.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CampaignApprovalRoutingModule } from './campaign-approval-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CampaignApprovalRoutingModule
+  ]
+})
+export class CampaignApprovalModule { }
