c86189ee6fd3983777bc2dda1d2d0a7beaa5c962
HenokZeg <henokzegeye1990@gmail.com>
2020-12-13 23:07:09 +0300

    Campaign detail page data fetched from the API instead of mock data

--- src/app/donation/components/container/campaign-donate/campaign-donate.component.ts
+++ src/app/donation/components/container/campaign-donate/campaign-donate.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../../../../environments/environment';
+import { CampaignService } from '../../../../campaign/state/campaign.service';
 
 @Component({
   selector: 'app-campaign-donate',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-donate.component.scss']
 })
 export class CampaignDonateComponent implements OnInit {
-
-  constructor() { }
+  campaign;
+  coverPhotoPath;
+  constructor(private campaignService: CampaignService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(queryParams=>{
+      const id = queryParams['id'];
+      this.campaignService.show(id).subscribe(res=>{
+        this.campaign = res;
+        this.coverPhotoPath = `${environment.apiUrl}/${this.campaign.campaign_imageUrl}`;
+      });
+    });
   }
 
 }
