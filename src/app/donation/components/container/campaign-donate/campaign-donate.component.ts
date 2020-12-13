0ba77969fb569679b8a3ed4a0a84e69d29666eb1
HenokZeg <henokzegeye1990@gmail.com>
2020-12-14 00:34:38 +0300

    Donation dialog closed after the buttons clicked

--- src/app/donation/components/container/campaign-donate/campaign-donate.component.ts
+++ src/app/donation/components/container/campaign-donate/campaign-donate.component.ts
@@ -4,6 +4,7 @@ import { environment } from '../../../../../environments/environment';
 import { CampaignService } from '../../../../campaign/state/campaign.service';
 import { UtilService } from '../../../../shared/services/util.service';
 import { DonationFormComponent } from '../../ui/donation-form/donation-form.component';
+import { DonationService } from '../../../state/donation.service';
 
 @Component({
   selector: 'app-campaign-donate',
@@ -13,9 +14,11 @@ import { DonationFormComponent } from '../../ui/donation-form/donation-form.comp
 export class CampaignDonateComponent implements OnInit {
   campaign;
   coverPhotoPath;
+  dialogRef;
   constructor(private campaignService: CampaignService,
               private route: ActivatedRoute,
-              private utilService: UtilService) { }
+              private utilService: UtilService,
+              private donationService: DonationService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(queryParams=>{
@@ -28,21 +31,25 @@ export class CampaignDonateComponent implements OnInit {
   }
 
   onDonate() {
-    const dialogRef = this.utilService.openDialog(DonationFormComponent,undefined, '500px');
-    const submitSub = (dialogRef.componentInstance as any).formSubmit.subscribe(data => {
+    this.dialogRef = this.utilService.openDialog(DonationFormComponent,undefined, '500px');
+    const submitSub = (this.dialogRef.componentInstance as any).formSubmit.subscribe(data => {
       this.onSubmit(data);
+      this.dialogRef.close();
     });
 
-    const closeSub = (dialogRef.componentInstance as any).formClose.subscribe(() => dialogRef.close());
+    const closeSub = (this.dialogRef.componentInstance as any).formClose.subscribe(() => this.dialogRef.close());
 
-    dialogRef.afterClosed().subscribe(() => {
+    this.dialogRef.afterClosed().subscribe(() => {
       submitSub.unsubscribe();
       closeSub.unsubscribe();
     });
   }
 
   onSubmit(data) {
-    console.log('dddd', data);
+    const campaignId = this.campaign._id;
+    this.donationService.donate(campaignId, data).subscribe(res=>{
+      //Update state based on the donation amount
+    });
   }
 
 }
--- src/app/donation/state/donation.service.ts
+++ src/app/donation/state/donation.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { UtilService } from '../../shared/services/util.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonationService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private utilService: UtilService) { }
+
+  donate(campaignId, data) {
+    const url = `${environment.apiUrl}/donate/${campaignId}`;
+    return this.http.post(url, data)
+      .pipe(
+        tap((result: any) => {
+          //Update store
+        }, error => {
+          console.log('error');
+        })
+      );
+  }
 }
