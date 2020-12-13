ce89d1ff82468f006b71a0b8e6d05bc74b1138dc
HenokZeg <henokzegeye1990@gmail.com>
2020-12-13 21:32:34 +0300

    Campaigns service added

--- src/app/components/container/explored-campaigns/explored-campaigns.component.ts
+++ src/app/components/container/explored-campaigns/explored-campaigns.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-explored-campaigns',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explored-campaigns.component.scss']
 })
 export class ExploredCampaignsComponent implements OnInit {
-
-  constructor() { }
+  category;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      this.category = param.category;
+      //Fetch campaign using category
+    });
   }
 
 }
